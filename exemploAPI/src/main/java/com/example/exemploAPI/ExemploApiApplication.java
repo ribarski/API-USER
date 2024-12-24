93ed2f45ce67dc8cc25bf7b93599e77f6c6da422
ribarski <eduardoribarski14@gmail.com>
2024-12-24 09:18:29 -0300

    api

---
+++ exemploAPI/src/main/java/com/example/exemploAPI/ExemploApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.exemploAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExemploApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExemploApiApplication.class, args);
+	}
+
+}
